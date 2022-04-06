03f8c3589039801b544bb7e5d203c1139ac40dc3
John Guzzetta <johnguzzettaiv@gmail.com>
2022-04-06 19:36:04 -0400

    Added hour blocks to HTML, added variables for each block

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
+// Variables to select the hour block sections
+var nine = document.querySelector("#hour-nine")
+var ten = document.querySelector("#hour-ten")
+var eleven = document.querySelector("#hour-eleven")
+var twelve = document.querySelector("#hour-twelve")
+var one = document.querySelector("#hour-one")
+var two = document.querySelector("#hour-two")
+var three = document.querySelector("#hour-three")
+var four = document.querySelector("#hour-four")
+var five = document.querySelector("#hour-five")
+// ------------------------------------------------------------
+
 // Sets the date and time
 var time = document.querySelector("#timeNow");
 var timeNow = moment().format("dddd, MMMM Do YYYY h:mm:ss a")
@@ -6,4 +18,4 @@ var update = function(){
     time.innerHTML = moment().format("dddd, MMMM Do YYYY [at] h:mm:ss a")
 }
 setInterval(update, 1000);
-// -----------------------------
+// ------------------------------------------------------------
